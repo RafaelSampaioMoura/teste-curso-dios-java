2d969515b9514ae3cc0d7411c12cf43ee4c6c9c5
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2023-03-17 00:06:14 -0300

    First commit

---
+++ first-java-project/src/com/rafael/FirstProject.java
@@ -0,0 +1,15 @@
+package com.rafael;
+
+import com.rafael.model.Gato;
+
+public class FirstProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Gato gato = new Gato("Paçoca", "Oreo", 3);
+		
+		System.out.println("Hello World!");
+	}
+
+}
